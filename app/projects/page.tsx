0d29b51667e2d3a4f43e600253fd04eff6a7b04d
jallen98 <jallen.4231@gmail.com>
2024-07-02 03:13:34 -0400

    Added model exploder project

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,6 +3,14 @@ import { ProjectCard, ProjectCardData } from "../ui/project-card";
 export default function Projects() {
     const projects: ProjectCardData[] = [
         {
+            title: "Three.js Model Exploder",
+            blurb: "View a model with orbit controls, drag the slider to explode the model into its parts. More features coming!",
+            imageProps: { src: "/exploder-demo.png", alt: "Exploder App Screenshot", priority: true },
+            tags: ['TypeScript', 'React', 'Three.js', 'React Three Fiber', 'GLTF'],
+            sourceUrl: "https://github.com/jallen98/model-exploder",
+            appUrl: "https://model-exploder.vercel.app/"
+        },
+        {
             title: "Next.js Invoicing",
             blurb: "Add, edit, and delete invoices from a PostgreSQL database. Mark invoices as paid or unpaid. Created with the Next.js tutorial.",
             imageProps: { src: "/invoice-demo.jpg", alt: "Invoice App Screenshot", priority: true },
@@ -16,11 +24,11 @@ export default function Projects() {
             imageProps: { src: "/lovelyengine.png", alt: "Lovely Engine Screenshot", priority: true },
             tags: ['C++', 'OpenGL', 'GLSL', 'gtest', 'CMake'],
             sourceUrl: "https://github.com/jallen98/LovelyEngine"
-        }
+        },
     ];
 
     return (
-        <div className="flex flex-col md:flex-row justify-between w-full">
+        <div className="grid grid-cols-2">
             { projects.map((data) => <div className="mb-10 flex justify-center" key={data.title}><ProjectCard data={data} /></div>) }
         </div>
     );
